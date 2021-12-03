b4ef70c04211885e0022ce49c4942708d227ff91
Joe <joelting03@gmail.com>
2021-12-04 01:44:32 +0800

    Add movement and rotation

---
+++ Top-down-shooting-game/Assets/InputMaster.cs
@@ -0,0 +1,331 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/InputMaster.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputMaster : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputMaster()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputMaster"",
+    ""maps"": [
+        {
+            ""name"": ""Player "",
+            ""id"": ""02a8247b-f249-4ec4-94d4-80bd85790b57"",
+            ""actions"": [
+                {
+                    ""name"": ""Shooting"",
+                    ""type"": ""Button"",
+                    ""id"": ""65a479b7-4ce6-431b-9f9a-437be9f6b517"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""9989017d-bb0e-43a8-9c9b-61f952dbe3ae"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": ""NormalizeVector2"",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""MouseDirection "",
+                    ""type"": ""Value"",
+                    ""id"": ""3f5a9633-e1f3-48e4-8af2-c91c0e54bdb9"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""JoystickDirection"",
+                    ""type"": ""Value"",
+                    ""id"": ""e2846acb-8bd6-4d8b-ade5-9bf494c3ea0e"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""9abc1746-c427-41a2-a0e9-66926464b012"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Shooting"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""40951d44-76a5-4b88-bcd6-f748fa73e9a5"",
+                    ""path"": ""<Gamepad>/rightStickPress"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Shooting"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""de57b772-7f7d-40a9-902e-9a0eaf18f87e"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""3e55dfea-170c-4608-b3e0-18b220ae203e"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""67a09ef2-4de2-4a84-8ea9-617ee2e0a2dc"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""13fc0883-f228-4b1e-a5f2-e41983013d3e"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""4fd9de7e-7b31-4e5c-aa45-2bec5ca37ede"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""670a7e27-3bdf-44a7-9393-3043fd7b3321"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""8cc9000d-036d-49b2-9f9c-c8689a164069"",
+                    ""path"": ""<Mouse>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""MouseDirection "",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""cb817feb-9fe2-4a12-9e01-e9e384064962"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""JoystickDirection"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard"",
+            ""bindingGroup"": ""Keyboard"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Gamepad"",
+            ""bindingGroup"": ""Gamepad"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Gamepad>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Player 
+        m_Player = asset.FindActionMap("Player ", throwIfNotFound: true);
+        m_Player_Shooting = m_Player.FindAction("Shooting", throwIfNotFound: true);
+        m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+        m_Player_MouseDirection = m_Player.FindAction("MouseDirection ", throwIfNotFound: true);
+        m_Player_JoystickDirection = m_Player.FindAction("JoystickDirection", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player 
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Shooting;
+    private readonly InputAction m_Player_Movement;
+    private readonly InputAction m_Player_MouseDirection;
+    private readonly InputAction m_Player_JoystickDirection;
+    public struct PlayerActions
+    {
+        private @InputMaster m_Wrapper;
+        public PlayerActions(@InputMaster wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Shooting => m_Wrapper.m_Player_Shooting;
+        public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputAction @MouseDirection => m_Wrapper.m_Player_MouseDirection;
+        public InputAction @JoystickDirection => m_Wrapper.m_Player_JoystickDirection;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Shooting.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShooting;
+                @Shooting.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShooting;
+                @Shooting.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShooting;
+                @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @MouseDirection.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMouseDirection;
+                @MouseDirection.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMouseDirection;
+                @MouseDirection.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMouseDirection;
+                @JoystickDirection.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJoystickDirection;
+                @JoystickDirection.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJoystickDirection;
+                @JoystickDirection.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJoystickDirection;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Shooting.started += instance.OnShooting;
+                @Shooting.performed += instance.OnShooting;
+                @Shooting.canceled += instance.OnShooting;
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @MouseDirection.started += instance.OnMouseDirection;
+                @MouseDirection.performed += instance.OnMouseDirection;
+                @MouseDirection.canceled += instance.OnMouseDirection;
+                @JoystickDirection.started += instance.OnJoystickDirection;
+                @JoystickDirection.performed += instance.OnJoystickDirection;
+                @JoystickDirection.canceled += instance.OnJoystickDirection;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    private int m_KeyboardSchemeIndex = -1;
+    public InputControlScheme KeyboardScheme
+    {
+        get
+        {
+            if (m_KeyboardSchemeIndex == -1) m_KeyboardSchemeIndex = asset.FindControlSchemeIndex("Keyboard");
+            return asset.controlSchemes[m_KeyboardSchemeIndex];
+        }
+    }
+    private int m_GamepadSchemeIndex = -1;
+    public InputControlScheme GamepadScheme
+    {
+        get
+        {
+            if (m_GamepadSchemeIndex == -1) m_GamepadSchemeIndex = asset.FindControlSchemeIndex("Gamepad");
+            return asset.controlSchemes[m_GamepadSchemeIndex];
+        }
+    }
+    public interface IPlayerActions
+    {
+        void OnShooting(InputAction.CallbackContext context);
+        void OnMovement(InputAction.CallbackContext context);
+        void OnMouseDirection(InputAction.CallbackContext context);
+        void OnJoystickDirection(InputAction.CallbackContext context);
+    }
+}
--- Top-down-shooting-game/Assets/Scripts/Player.cs
+++ Top-down-shooting-game/Assets/Scripts/Player.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody rb;
+    private InputMaster Controls;
+    [SerializeField]
+    private float Speed;
+    [SerializeField]
+    private bool MouseRotating;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        Controls = new InputMaster();
+
+    }
+    private void FixedUpdate()
+    {
+        MoveMent();
+        if (MouseRotating)
+        {
+            MouseRotation();
+        }
+        else
+        {
+            JoystickRotation();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MoveMent()
     {
-        
+        Vector2 InputVector = Controls.Player.Movement.ReadValue<Vector2>();
+        rb.MovePosition(new Vector3(InputVector.x, 0, InputVector.y) * Speed * Time.deltaTime + rb.position);
+    }
+    private void JoystickRotation()
+    {
+        Vector2 InputVector = Controls.Player.JoystickDirection.ReadValue<Vector2>();
+        float TurnAngle = Mathf.Atan2(InputVector.y, InputVector.x) * Mathf.Rad2Deg + 90f;
+        transform.rotation = Quaternion.Euler(new Vector3(0f, -TurnAngle, 0f));
+    }
+    private void MouseRotation()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Controls.Player.MouseDirection.ReadValue<Vector2>());
+        if (Physics.Raycast(ray, out RaycastHit hitinfo, 300f))
+        {
+            Vector3 Target = hitinfo.point;
+            Vector3 LookDirection = Target - transform.position;
+            float TurnAngle = Mathf.Atan2(LookDirection.z, LookDirection.x) * Mathf.Rad2Deg + 90f;
+            transform.rotation = Quaternion.Euler(new Vector3(0f, -TurnAngle, 0f));
+        }
+    }
+    private void OnEnable()
+    {
+        Controls.Enable();
+    }
+    private void OnDisable()
+    {
+        Controls.Disable();
     }
 }
